9551d4f1fc955979ab33ed2a893e0df27c6ccefd
Dev2-ops2 <dev2ops2@hotmail.com>
2024-05-02 21:41:10 +0200

    shtim autofocus te tagu input dhe shtim funksionaliteti qe duke shtypur nga tastera enter teksti te kaloj automatikisht te lista to do

--- script.js
+++ script.js
@@ -1,71 +1,55 @@
-// Veprimet qe do kryejme kur ngarkohet faqia
-
-// Zgjedh elementet qe na duhen nga faqia
 const button = document.querySelector('button');
-const inputField = document.querySelector('input');
-const emptyListMessage = document.querySelector('.empty-list-message');
-const todoList = document.querySelector('ul');
-// const footer = document.querySelector('footer');
+const inputText = document.querySelector('input');
+const toDoList = document.querySelector('ul');
+const tekstSkaPune = document.querySelector('.empty-list-message');
 
-// Pergatis listen me aktivitete 
-let activities = [];
-const local_storage_activities_key = 'activities';
+let punet = [];
+showContent();
 
-let activities_from_localStorage = localStorage.getItem('local_storage_activities_key');
-if (activities_from_localStorage) {
-  activities = JSON.parse(activities_from_localStorage);
-}
-showContent()
+inputText.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    let newActivity = inputText.value.trim();
+    if (newActivity.length > 0) {
+      punet.push(newActivity);
+      showContent();
+      inputText.value = '';
+    }
+  }
+})
+button.addEventListener('click', function () {
+  let newActivity = inputText.value.trim();
+  if (newActivity.length > 0) {
+    punet.push(newActivity);
+    showContent();
+    inputText.value = '';
+  }
 
-//VEPRIME DINAMIKE
-button.addEventListener('click', () => addActivity());
-
+})
 
 function showContent() {
-  emptyListMessage.innerText = '';
-  todoList.innerText = '';
+  toDoList.innerText = '';
+  tekstSkaPune.innerText = '';
+  if (punet.length > 0) {
 
-  if (activities.length > 0) {
-    //shfaq aktivitete
-    for (let i = 0; i < activities.length; i++) {
-      todoList.innerHTML += `
-      <li class="to-do-item">
-        <div class="to-do-check">
-          <img src="images/check.svg" alt="Check Icon">
-        </div>
-        <p class="to-do-text">${activities[i]}</p>
-      </li>
-      `;
+    for (let i = 0; i < punet.length; i++) {
+      toDoList.innerHTML += `<li class="to-do-item">
+    <div class="to-do-check">
+    <img src="images/check.svg" alt="Check Icon">
+    </div>
+    <p class="to-do-text">${punet[i]}</p>
+</li>`
     }
-    console.log(activities);
-    removeItem();
+
+    const checks = document.querySelectorAll('.to-do-check');
+    for (let i = 0; i < checks.length; i++) {
+      checks[i].addEventListener('click', function () {
+        punet.splice(i, 1);
+        showContent();
+      })
+    }
   } else {
-    emptyListMessage.innerText = 'Nuk ka asnje aktivitet';
+    tekstSkaPune.innerText = 'Ska asnje aktivitet'
   }
-}
 
-function addActivity() {
-  const newActivity = inputField.value.trim();
-  if (newActivity.length > 0) {
-    activities.push(newActivity);
-    inputField.value = '';
-    showContent();
-    localStorage.setItem('local_storage_activities_key', JSON.stringify(activities));
-  }
 }
-
-function removeItem() {
-  const checks = document.querySelectorAll('.to-do-check');
-  console.log(checks)
-  for (let i = 0; i < checks.length; i++) {
-    checks[i].addEventListener('click', function () {
-      console.log(`u klikua check me index ${i}`)
-      activities.splice(i, 1);
-      localStorage.setItem('local_storage_activities_key', JSON.stringify(activities));
-      showContent();
-      console.log(activities);
-    })
-  }
-}
-
 
